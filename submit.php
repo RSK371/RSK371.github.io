f191ea6ce08fb2177b00e5a17580c219f3c28da3
RSK371 <46217867+RSK371@users.noreply.github.com>
2018-12-28 12:09:10 +0200

    Update submit.php

--- submit.php
+++ submit.php
@@ -10,11 +10,9 @@ $weight = filter_var($_POST['Weight'], FILTER_SANITIZE_NUMBER_INT);
 $height = filter_var($_POST['Height'], FILTER_SANITIZE_NUMBER_INT);
 $width = filter_var($_POST['Width'], FILTER_SANITIZE_NUMBER_INT);
 $length = filter_var($_POST['Length'], FILTER_SANITIZE_NUMBER_INT);
-$catg = filter_var($_POST['Category'], FILTER_SANITIZE_NUMBER_INT);
-
+$catg = $_POST['Category'];
 $sql = "INSERT INTO articles (SKU,Name1,Image,Price,Category,Size,Weight,Height,Width,Length) VALUES ('$sku','$name','$image','$price','$catg','$size','$weight',
 '$height','$width','$length')";
-
 if(!mysqli_query($con,$sql)){
     echo 'Not Inserted';
 } else {
