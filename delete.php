129f9d3ac97ffc16f91cceaf2df477ddfae4acf5
RSK371 <46217867+RSK371@users.noreply.github.com>
2018-12-28 11:57:46 +0200

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,6 +1,6 @@
 <?php
-    header("refresh:1; url=index.php"), FILTER_SANITIZE_NUMBER_INT);
-    $sku = filter_var($_POST['id'];
+    header("refresh:1; url=index.php")
+    $sku = filter_var($_POST['id'], FILTER_SANITIZE_NUMBER_INT);
     $connect = mysqli_connect('localhost','root','','add_product');
     $query = "DELETE FROM `articles` WHERE `SKU` = $sku";
     $result = mysqli_query($connect, $query);
