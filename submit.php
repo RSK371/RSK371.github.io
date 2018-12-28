772671f5926bd3e9624ab4d9e84afd92b855ff84
RSK371 <46217867+RSK371@users.noreply.github.com>
2018-12-28 11:50:12 +0200

    Update submit.php

--- submit.php
+++ submit.php
@@ -2,15 +2,15 @@
 header("refresh:1; url=index.php");
 $msg = "";
 include('includes/db.php');
-$sku = $_POST['sku'];
-$name = $_POST['Name'];
-$price = $_POST['Price'];
-$size = $_POST['Size'];
-$weight = $_POST['Weight'];
-$height = $_POST['Height'];
-$width = $_POST['Width'];
-$length = $_POST['Length'];
-$catg = $_POST['Category'];
+$sku = filter_var($_POST['sku'], FILTER_SANITIZE_NUMBER_INT);
+$name = filter_var($_POST['Name'], FILTER_SANITIZE_STRING);
+$price = filter_var($_POST['Price'], FILTER_SANITIZE_NUMBER_INT);
+$size = filter_var($_POST['Size'], FILTER_SANITIZE_NUMBER_INT);
+$weight = filter_var($_POST['Weight'], FILTER_SANITIZE_NUMBER_INT);
+$height = filter_var($_POST['Height'], FILTER_SANITIZE_NUMBER_INT);
+$width = filter_var($_POST['Width'], FILTER_SANITIZE_NUMBER_INT);
+$length = filter_var($_POST['Length'], FILTER_SANITIZE_NUMBER_INT);
+$catg = filter_var($_POST['Category'], FILTER_SANITIZE_NUMBER_INT);
 
 $sql = "INSERT INTO articles (SKU,Name1,Image,Price,Category,Size,Weight,Height,Width,Length) VALUES ('$sku','$name','$image','$price','$catg','$size','$weight',
 '$height','$width','$length')";
